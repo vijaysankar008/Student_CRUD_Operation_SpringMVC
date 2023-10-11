994dd3a6f760b90cae021de41c4ce5e061fc9929
VJ <you@example.com>
2023-10-11 14:35:20 +0530

    Third_Commit

--- Student_CRUD_operations/src/main/java/com/aiht/students_data/controller/StudentController.java
+++ Student_CRUD_operations/src/main/java/com/aiht/students_data/controller/StudentController.java
@@ -5,17 +5,21 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.aiht.students_data.domain.Student;
 import com.aiht.students_data.service.StudentService;
 
 @Controller
+@RequestMapping("/studentDatabase")
 public class StudentController {
 
 	@Autowired
@@ -27,7 +31,6 @@ public class StudentController {
 	public String homePage(Model model) {
 		List<Student> studentsList = studentService.listAll();
 		model.addAttribute("studentsList", studentsList);
-		// System.out.print("Get / ");
 		return "Index";
 	}
 
@@ -40,20 +43,27 @@ public class StudentController {
 	@PostMapping("/save")
 	public String saveStudent(@ModelAttribute("student") Student student) {
 		studentService.saveStudentData(student);// It will store the data to the database
-		return "redirect:/home";
+		return "redirect:/studentDatabase/home";
 	}
 
-	@RequestMapping("/edit/{id}")
-	public ModelAndView showEditStudentPage(@PathVariable(name = "id") int id) {
-		ModelAndView view = new ModelAndView("new");
-		Student student = studentService.editStudentData(id);
-		view.addObject("student", student);
-		return view;
+	// Display the form to edit a student's data
+	@GetMapping("/edit/{id}")
+	public String showEditStudentPage(@PathVariable(name = "id") int id, Model model) {
+		Student student = studentService.getStudentById(id);
+		model.addAttribute("student", student);
+		return "editStudent"; // Display the edit form
 	}
-      
-	@RequestMapping("/delete/{id}")
-	public String deleteStudent(@PathVariable(name="id") int id) {
+
+	// Handle the form submission to update a student's data
+	@PostMapping("/edit/{id}")
+	public String updateStudentData(@PathVariable(name = "id") int id, @ModelAttribute Student student) {
+		studentService.updateStudentData(id, student);
+		return "redirect:/studentDatabase/home"; // Redirect to a student list page or any other appropriate page
+	}
+
+	@GetMapping("/delete/{id}")
+	public String deleteStudent(@PathVariable(name = "id") int id) {
 		studentService.deleteData(id);
-		return "redirect:/home";
+		return "redirect:/studentDatabase/home";
 	}
 }
--- Student_CRUD_operations/src/main/java/com/aiht/students_data/service/StudentService.java
+++ Student_CRUD_operations/src/main/java/com/aiht/students_data/service/StudentService.java
@@ -1,6 +1,7 @@
 package com.aiht.students_data.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -23,11 +24,31 @@ public class StudentService {
 		return studentRepository.findAll();
 	}
 
-    public Student editStudentData(int id) {
+    public Student getStudentById(int id) {
     	return studentRepository.findById(id).get();
     }
     
     public void deleteData(int id) {
     	studentRepository.deleteById(id);
+    }
+
+    public void updateStudentData(int id, Student updatedStudent) {
+        // Retrieve the existing student by ID
+        Optional<Student> existingStudent = studentRepository.findById(id);
+        
+        if (existingStudent.isPresent()) {
+            Student studentToUpdate = existingStudent.get();
+            
+            // Update the student's properties with the new data
+            studentToUpdate.setName(updatedStudent.getName());
+            studentToUpdate.setCourse(updatedStudent.getCourse());
+            studentToUpdate.setFees(updatedStudent.getFees());
+
+            // Save the updated student to the database
+            studentRepository.save(studentToUpdate);
+        } else {
+            // Handle the case where the student with the given ID is not found
+            // You can throw an exception or handle it in a way that suits your application
+        }
     }
 }
